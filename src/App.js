cbdca6128d9c3e246584741327e8e2b12111c9ba
Nuno Neves <youknownuno@gmail.com>
2018-06-22 18:22:59 +0100

    maps boiler plate for react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
+import Map from './Map'
 class App extends Component {
   render() {
     return (
@@ -13,9 +14,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Map
+          isMarkerShown={false}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Map.js
+++ src/Map.js
@@ -1,1 +1,15 @@
-Map.js
+import React, { Component } from 'react'
+
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const Map = withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+)
+
+
+export default Map
