99e5d4273baeb57d13de0f3d6603e8c2ae9c6850
Nuno Neves <youknownuno@gmail.com>
2018-06-23 19:37:18 +0100

    refactored App dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
+// fake data
 import defaultOptions from './mapOptions.js'
 import locations from './locations.js'
+
+// custom component
 import ReactMap from './ReactMap.js'
+
+// materialUI elements
+import VictimUI from './VictimUI.js'
 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">OhShit is here to help</h1>
-        </header>
-
+        <VictimUI/>
         <ReactMap
           isMarkerShown={false}
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
           mapElement={<div style={{ height: `100%` }} />}
           defaultOptions={defaultOptions}
           locations={locations}
