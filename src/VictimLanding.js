1f8e827066bea4c95f25829ac18a5b4910a28431
Nuno Neves <youknownuno@gmail.com>
2018-06-26 22:30:42 +0100

    Created landing page component for victims and volunteers

---
+++ src/VictimLanding.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ProgressBar from './ProgressBar.js'
+
+const VictimLanding = () => {
+	return (
+		<div className='landing'>
+			<h3>Get-A-Way</h3>
+			<p>Don't worry,<br/>You're on our radar now</p>
+			<ProgressBar/>
+		</div>
+	)
+}
+
+export default VictimLanding
---
+++ src/VolunteerLanding.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ProgressBar from './ProgressBar.js'
+
+const VolunteerLanding = () => {
+	return (
+		<div className='landing'>
+			<h3>Get-A-Way</h3>
+			<p>You're a hero.</p>
+			<ProgressBar/>
+		</div>
+	)
+}
+
+export default VolunteerLanding
