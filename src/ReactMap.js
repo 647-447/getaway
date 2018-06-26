3e7706081ba74af19cd8733219d7d8720a33dd65
Nuno Neves <youknownuno@gmail.com>
2018-06-26 22:29:11 +0100

    Refactored to render clusters conditionally based on props.victims

--- src/ReactMap.js
+++ src/ReactMap.js
@@ -3,13 +3,14 @@ import { withGoogleMap, GoogleMap } from "react-google-maps"
 
 import ReactMarkerCluster from './ReactMarkerCluster.js'
 
-const ReactMap = withGoogleMap((props) =>
+const ReactMap = withGoogleMap( (props) =>
   <GoogleMap
     defaultOptions={props.defaultOptions}
   >
-  <ReactMarkerCluster
+  { props.victims ?( <ReactMarkerCluster
   	victims={props.victims}
-  />
+  /> ) : null
+}
   </GoogleMap>
 )
 
